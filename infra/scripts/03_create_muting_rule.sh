07d5eb6f420b427f50047f9029bd5aedc15a775f
uturkarslan <ugurturkarslan@gmail.com>
2022-07-11 13:41:15 +0300

    Implement muting rule creation

--- infra/scripts/03_create_muting_rule.sh
+++ infra/scripts/03_create_muting_rule.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-accountId="<YOUR_ACCOUNT_ID>"
+### Set variables ###
+
+# Account
+accountId="YOUR_ACCOUNT_ID"
+
+# Alert condition
+alertConditionId="YOUR_ALERT_CONDITION_ID"
+
+# Muting Rule
 mutingRuleName="Shut the hell up"
-alertConditionId="<YOUR_ALERT_CONDITION_ID>"
 
 # Time Format -> "YYYY-MM-DDThh:mm:ss" (e.g. "2022-07-10T14:00:00")
-startTime="<YOUR_START_TIME>"
-endTime="<YOUR_END_TIME>"
+startTime="YOUR_START_TIME"
+endTime="YOUR_END_TIME"
+timeZone="YOUR_TIME_ZONE"
 
-curl https://api.eu.newrelic.com/graphql \
+###################
+### Muting Rule ###
+###################
+
+# Set NerdGraph query
+query='{"query":"mutation {\n  alertsMutingRuleCreate(accountId: "'$accountId'", rule: {condition: {conditions: {attribute: \"conditionId\", operator: EQUALS, values: \"'$alertConditionId'\"}, operator: AND}, enabled: true, name: \"'$mutingRuleName'\", schedule: {startTime: \"'$startTime'\", endTime: \"'$endTime'\", timeZone: \"'$timeZone'\"}}) {\n    id\n  }\n}\n", "variables":""}'
+
+# Clear the additional spaces
+query=$(echo $query | sed 's/    /  /g')
+
+mutingRule=$(curl https://api.eu.newrelic.com/graphql \
   -H "Content-Type: application/json" \
   -H "API-Key: $NEWRELIC_LICENSE_KEY" \
-  --data-binary '{"query":"mutation {\n  alertsMutingRuleCreate(accountId: "'"$accountId"'", rule: {condition: {conditions: {attribute: \"conditionId\", operator: EQUALS, values: \"1571667\"}, operator: AND}, enabled: true, name: \""'"$mutingRuleName"'"\", schedule: {startTime: \"2022-07-10T14:00:00\", endTime: \"2022-07-10T15:00:00\", timeZone: \"Europe/Berlin\"}}) {\n    id\n  }\n}\n", "variables":""}'
+  --data-binary "$query" \
+  | jq)
+
+echo $mutingRule
+#########
